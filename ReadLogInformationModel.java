2e5a267ff8de2b518e90f21448e60b7aecc3f08f
Parmindersingh1750 <145669269+Parmindersingh1750@users.noreply.github.com>
2023-09-22 21:40:58 +0530

    Update ReadLogInformationModel.java

--- ReadLogInformationModel.java
+++ ReadLogInformationModel.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  * This the the model class that will read information for the dashboard. This
  * class will be used only for the reading from the database.
  *
- * @author Punit Citation:
+ * @author Parminder and Harkirat Citation:
  * https://www.mkyong.com/mongodb/java-mongodb-insert-a-document/
  * http://www.mkyong.com/mongodb/java-mongodb-hello-world-example/
  */
